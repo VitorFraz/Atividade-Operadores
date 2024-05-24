a0df4a99aac4598c73d7a18877748c1c948d6b56
Fraz <vfbarduco@gmail.com>
2024-05-24 09:22:48 -0300

    Atividade da Aula3

---
+++ Atividade Operadores.js
@@ -0,0 +1,70 @@
+console.log("=============Ex 1-=============")
+
+var numero01 = 4
+var numero02 = 5
+
+console.log("4 + 5 =", numero01 + numero02)
+console.log("5 - 4 =", numero02 - numero01)
+console.log("4 X 5 =", numero01 * numero02)
+console.log("5 / 4 =", numero02 / numero01)
+
+console.log("===============================")
+
+console.log("=============Ex 2-=============")
+
+//Formula F: C = (F - 32 ) / 1,8
+
+var numeroF = 50
+var valor01 = 32
+var valor02 = 1.8
+
+console.log("C =", (numeroF - valor01) / valor02, "C")
+
+console.log("===============================")
+
+console.log("=============Ex 3-=============")
+
+// Formula C: F = C x (9/5) + 32
+
+var numeroC = 20
+var valor03 = 9
+var valor04 = 5
+var valor05 = 32
+
+
+console.log("F =", numeroC * (valor03 / valor04) + valor05, "F")
+
+console.log("===============================")
+
+console.log("=============Ex 4-=============")
+
+// Para transformar M em CM, multiplique-se o valor de M por 100
+
+var numeroM = 8
+var valorC = 100
+
+console.log(numeroM * valorC, "cm")
+
+console.log("===============================")
+
+console.log("=============Ex 5-=============")
+
+//1º devemos calcular o valor resultante da multiplicação do produto com a porcentagem do desconto
+//2º depois subtrair o produto com o valor resultante para achar o preço final
+
+var valorP = 34.99
+var valorD = 10
+var valorPorcetagemD = 0.1
+var valorResultanteP = 3.5
+
+console.log(valorP * valorPorcetagemD)
+console.log(valorP - valorResultanteP, "R$")
+
+console.log("===============================")
+
+
+
+
+
+
+
